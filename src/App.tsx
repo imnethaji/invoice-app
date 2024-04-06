bde4394bde4df2ca783438d37eb11fe14611b57f
Nethaji P <reachnethajip@gmail.com>
2024-04-06 19:05:13 +0530

    Icon change

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,10 @@ import arrowIcon from "./assets/icon-arrow-down.svg";
 import emptyIllustration from "./assets/illustration-empty.svg";
 
 function App() {
+  function handleFilterClick() {
+    console.log("works");
+  }
+
   return (
     <>
       <div>
@@ -16,13 +20,16 @@ function App() {
               </div>
             </div>
             <div className="flex items-center">
-              <p className="flex  items-center text-white font-bold cursor-pointer">
+              <p
+                onClick={handleFilterClick}
+                className="flex  items-center text-white font-bold cursor-pointer"
+              >
                 Filter by status{" "}
                 <span className="ml-5">
                   <img src={arrowIcon} alt="" />
                 </span>
               </p>
-              <button className=" flex items-center font-bold text-white bg-buttonPurple text-white rounded-full ml-5 p-2 px-3">
+              <button className=" flex items-center font-bold text-white bg-buttonPurple rounded-full ml-5 p-2 px-3">
                 <div className="w-10 h-10 mr-3 flex items-center justify-center bg-white rounded-full">
                   <img src={plusIcon} alt="" className="w-3 h-3" />
                 </div>
